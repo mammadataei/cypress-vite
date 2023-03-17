4f68767ea56d3e51d1524984e02da7e99a78d796
Mohammad Ataei <mammadataei@gmail.com>
2023-03-17 20:51:30 +0330

    chore: use vite-tsconfig-paths for resolving aliases

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,19 +1,7 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { compilerOptions } from './tsconfig.json'
-
-function resolveAliasesFromTypescriptConfig() {
-  return Object.entries(compilerOptions.paths).reduce((acc, [key, [value]]) => {
-    const aliasKey = key.substring(0, key.length - 2)
-    const path = value.substring(0, value.length - 2)
-    return { ...acc, [aliasKey]: resolve(__dirname, path) }
-  }, {})
-}
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  resolve: {
-    alias: resolveAliasesFromTypescriptConfig(),
-  },
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
 })
