baa3d8d115b26d8961f55d495b47f3768dfcb4f8
Blake Vandercar <blake.vandercar@validatek.com>
2026-05-02 19:37:20 -0600

    chore(deps): update tailwind to v4

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [tailwindcss(), react(), tsconfigPaths()],
 })
