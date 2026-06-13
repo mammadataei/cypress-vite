0999d930a2842a28e61220f877824ceeb1947862
Blake Vandercar <blake.vandercar@validatek.com>
2026-06-13 12:53:29 -0600

    chore: updates

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [tailwindcss(), react(), tsconfigPaths()],
 })
