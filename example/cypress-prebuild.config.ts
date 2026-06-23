a084a4bdfd432c5fc45db04d619f6c9c554c5995
bvandercar-vt <bvandercar@outlook.com>
2026-06-23 12:21:15 -0600

    test: move to better location

--- example/cypress-prebuild.config.ts
+++ example/cypress-prebuild.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     baseUrl: 'http://localhost:5173/',
     viewportWidth: 1280,
     viewportHeight: 768,
-    specPattern: '**/*.e2e.ts',
+    specPattern: 'cypress/**/*.e2e.ts',
     video: false,
     screenshotOnRunFailure: false,
 
